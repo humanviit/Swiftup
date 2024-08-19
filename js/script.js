e8b2cbfa24606c6d79688867cfd72bdf9d5891e7
Pavan <pavankumar41483@gmail.com>
2024-08-20 00:13:29 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -62,3 +62,17 @@ window.onscroll = () =>{
       body.classList.remove('active');
    }
 }
+
+
+
+
+// JavaScript for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+   anchor.addEventListener('click', function(e) {
+       e.preventDefault();
+
+       document.querySelector(this.getAttribute('href')).scrollIntoView({
+           behavior: 'smooth'
+       });
+   });
+});
