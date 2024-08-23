4804a491bb74c71ac3a86b7a095cac71e19fe12f
Pavan <pavankumar41483@gmail.com>
2024-08-23 10:49:22 +0530

    Update

---
+++ footer.php
@@ -0,0 +1,31 @@
+<!-- footer.php -->
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-left">
+            <h2>Company</h2>
+            <p>Your company's info here</p>
+        </div>
+        <div class="footer-center">
+            <h2>Quick Links</h2>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#course">Courses</a></li>
+                <li><a href="#call">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-right">
+            <h2>Social Media</h2>
+            <div class="social-links">
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Instagram</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Swiftup. All Rights Reserved.</p>
+    </div>
+</footer>
+
+<script src="js/script.js"></script>
